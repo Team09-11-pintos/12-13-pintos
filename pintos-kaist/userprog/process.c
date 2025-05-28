04785a1c0b68084e5bd2fe146661f163512707fc
JeonghwanY <jh327373@gmail.com>
2025-05-28 16:00:18 +0900

    bbungjun

--- pintos-kaist/userprog/process.c
+++ pintos-kaist/userprog/process.c
@@ -21,7 +21,7 @@
 #ifdef VM
 #include "vm/vm.h"
 #endif
-
+//
 static void process_cleanup (void);
 static bool load (const char *file_name, struct intr_frame *if_);
 static void initd (void *f_name);
